f9e8590c2dd690572797d440decc72ff62879052
abol ghana <aghp81@gmail.com>
2022-12-28 11:01:48 +0330

    custom.css for RTL & @vite

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -11,10 +11,13 @@
         <link rel="stylesheet" href="https://fonts.bunny.net/css2?family=Nunito:wght@400;600;700&display=swap">
 
         <!-- Scripts -->
-        @vite(['resources/css/app.css', 'resources/js/app.js'])
-
+        @vite(['resources/css/app.css', 'resources/css/custom.css', 'resources/js/app.js'])
+        
         <!-- Styles -->
         @livewireStyles
+     
+
+       
     </head>
     <body class="font-sans antialiased">
         <x-jet-banner />
