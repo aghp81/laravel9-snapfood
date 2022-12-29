a5332328900d8fd34b2d02f8a8bc0f4a76da4ea3
abol ghana <aghp81@gmail.com>
2022-12-29 23:53:36 +0330

    make table for shop

--- database/migrations/2022_12_29_232353_create_shops_table.php
+++ database/migrations/2022_12_29_232353_create_shops_table.php
@@ -16,6 +16,11 @@ return new class extends Migration
         Schema::create('shops', function (Blueprint $table) {
             $table->id();
             $table->unsignedInteger('user_id'); // for user_id
+            $table->string('title');
+            $table->string('first_name');
+            $table->string('last_name');
+            $table->string('telephone');
+            $table->text('address')->nullable();
             $table->timestamps();
         });
     }
