72241d02bfeefe59bc14eb0d12d1517953f2b532
abol ghana <aghp81@gmail.com>
2022-12-30 00:28:28 +0330

    make content at app.blade.php - edit style

---
+++ resources/views/shop/index.blade.php
@@ -0,0 +1,9 @@
+<x-app-layout>
+    <x-slot name="header">
+        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+            {{ __('Sellers Management') }}
+        </h2>
+    </x-slot>
+
+    salam4
+</x-app-layout>
