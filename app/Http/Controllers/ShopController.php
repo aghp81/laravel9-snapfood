572c8eeca391934a4c2f94cd12949a29e35bbbf1
abol ghana <aghp81@gmail.com>
2022-12-30 17:24:57 +0330

    only & except in middelware

--- app/Http/Controllers/ShopController.php
+++ app/Http/Controllers/ShopController.php
@@ -11,9 +11,11 @@ class ShopController extends Controller
     // برای پیاده سازی میدلور CheckAdmin
     public function __construct()
     {
-        $this->middleware(['auth', 'admin'])->except(['show', 'index']);// خارج کردن دو بخش از میدلور. همه کاربران می توانند ببینند.
-        // $this->middleware('auth');
-        // $this->middleware('admin');
+        $this->middleware(['auth', 'admin']);
+        // $this->middleware(['auth', 'admin'])->except(['show', 'index']);// خارج کردن دو بخش از میدلور. همه کاربران می توانند ببینند.
+       // $this->middleware(['auth', 'admin'])->only(['show', 'index']);// فقط این دو بخش در میدلور ادمین باشند و سایر بخش ها را همه بتوانند ببینند.
+      // $this->middleware('auth');
+     // $this->middleware('admin');
     }
     
     public function index()
