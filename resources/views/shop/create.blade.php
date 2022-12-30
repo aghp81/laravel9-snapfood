e3db9d48582005a789d651503995720e9f729e6f
abol ghana <aghp81@gmail.com>
2022-12-30 11:11:07 +0330

    make form for shop.create

--- resources/views/shop/create.blade.php
+++ resources/views/shop/create.blade.php
@@ -1,12 +1,33 @@
 <x-app-layout>
     <x-slot name="header">
         <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-            {{ __('Sellers Management') }}
+            {{ __('Create New Sellers') }}
         </h2>
     </x-slot>
 
+    <form action="{{ route('shop.store') }}" method="POST">
 
-    sss
+        @csrf
+
+        <div class="my-2">
+            <x-jet-label for="title" value="{{ __('Title Of Store') }}" />
+            <x-jet-input id="title" class="block mt-3 w-full" type="text" name="title" :value="old('title')" required autofocus />
+        </div>
+
+        <div class="my-2">
+            <x-jet-label for="first_name" value="{{ __('first_name') }}" />
+            <x-jet-input id="first_name" class="block mt-3 w-full" type="text" name="first_name" :value="old('first_name')" required />
+        </div>
+
+        <div class="my-2">
+            <x-jet-label for="last_name" value="{{ __('last_name') }}" />
+            <x-jet-input id="last_name" class="block mt-3 w-full" type="text" name="last_name" :value="old('last_name')" required />
+        </div>
+
+        
+
+    </form>
+    
 
 
 </x-app-layout>
