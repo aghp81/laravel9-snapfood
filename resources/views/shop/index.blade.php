71b9e9d0af275c8513df16c05b9a9fad96e5c280
abol ghana <aghp81@gmail.com>
2022-12-30 10:41:18 +0330

    make button for create new shop or seller

--- resources/views/shop/index.blade.php
+++ resources/views/shop/index.blade.php
@@ -5,5 +5,21 @@
         </h2>
     </x-slot>
 
-    salam4
+
+    <div class="flex justify-end">
+
+        <a href="{{ route ('shop.create') }}" class="inline-flex items-center px-4 py-2 bg-gray-700 border border-transparent rounded-md font-semibold text-xs text-white uppercase tracking-widest hover:bg-gray-700 active:bg-gray-900 focus:outline-none focus:border-gray-900 focus:ring focus:ring-gray-300 disabled:opacity-25 transition">
+            {{ __('Define New Sellers') }}
+        </a>
+
+        
+        <a href=""></a>
+        <a href=""></a>
+        <a href=""></a>
+    </div>
+
+    
+    <hr class="my-4">
+
+
 </x-app-layout>
