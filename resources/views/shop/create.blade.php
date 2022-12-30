9b971a124423b092b4b45d5a5213f57788e7b88f
abol ghana <aghp81@gmail.com>
2022-12-30 11:33:20 +0330

    رفع ایراد tailwind css

--- resources/views/shop/create.blade.php
+++ resources/views/shop/create.blade.php
@@ -5,21 +5,21 @@
         </h2>
     </x-slot>
 
-    <form action="{{ route('shop.store') }}" method="POST">
+    <form class="grid grid-cols-3 gap-4" action="{{ route('shop.store') }}" method="POST">
 
         @csrf
 
-        <div class="my-2">
+        <div class="mb-4">
             <x-jet-label for="title" value="{{ __('Title Of Store') }}" />
             <x-jet-input id="title" class="block mt-3 w-full" type="text" name="title" :value="old('title')" required autofocus />
         </div>
 
-        <div class="my-2">
+        <div class="mb-4">
             <x-jet-label for="first_name" value="{{ __('first_name') }}" />
             <x-jet-input id="first_name" class="block mt-3 w-full" type="text" name="first_name" :value="old('first_name')" required />
         </div>
 
-        <div class="my-2">
+        <div class="mb-4">
             <x-jet-label for="last_name" value="{{ __('last_name') }}" />
             <x-jet-input id="last_name" class="block mt-3 w-full" type="text" name="last_name" :value="old('last_name')" required />
         </div>
