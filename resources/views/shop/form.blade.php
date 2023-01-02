77a8d0d0cc5352324e894ee358a7007657a232db
abol ghana <aghp81@gmail.com>
2023-01-02 22:46:23 +0330

    form.blade هم برای ایجاد و هم برای ویرایش فروشگاه

---
+++ resources/views/shop/form.blade.php
@@ -0,0 +1,74 @@
+<x-app-layout>
+    <x-slot name="header">
+        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+            {{ __('Create New Sellers') }}
+        </h2>
+    </x-slot>
+
+    
+
+    <form class="grid grid-cols-3 gap-4" action="{{ route('shop.store') }}" method="POST">
+
+        @csrf
+
+        <div>
+            <x-jet-label for="title" value="{{ __('Title Of Store') }}" />
+            <x-jet-input id="title" class="block mt-3 w-full" type="text" name="title" :value="old('title')" required autofocus />
+        </div>
+
+        <div>
+            <x-jet-label for="first_name" value="{{ __('first_name') }}" />
+            <x-jet-input id="first_name" class="block mt-3 w-full" type="text" name="first_name" :value="old('first_name')" required />
+        </div>
+
+        <div>
+            <x-jet-label for="last_name" value="{{ __('last_name') }}" />
+            <x-jet-input id="last_name" class="block mt-3 w-full" type="text" name="last_name" :value="old('last_name')" required />
+        </div>
+
+        <div>
+            <x-jet-label for="telephone" value="{{ __('telephone') }}" />
+            <x-jet-input id="telephone" class="block mt-3 w-full" type="text" name="telephone" :value="old('telephone')" required />
+        </div>
+
+        
+
+        
+        <!-- دریافت ایمیل و نام کاربری و آدرس-->
+        <div>
+            <div>
+                <x-jet-label for="email" value="{{ __('Email') }}" />
+                <x-jet-input id="email" class="block mt-3 w-full" type="text" name="email" :value="old('email')" required />
+            </div>
+        </div>
+
+        <div>
+            <div>
+                <x-jet-label for="username" value="{{ __('Username') }}" />
+                <x-jet-input id="username" class="block mt-3 w-full" type="text" name="username" :value="old('username')" required />
+            </div>
+        </div>
+
+        <div class="col-span-3">
+            <x-jet-label for="address" value="{{ __('address') }}" />
+            <x-jet-input id="address" class="block mt-3 w-full" type="text" name="address" :value="old('address')" />
+        </div>
+        
+
+        
+        <!-- دکمه ذخیره -->
+        <div  class="col-start-2 col-end-3">
+            <div class="flex justify-center">
+                <x-jet-button>
+                    {{ __('Save') }}
+                </x-jet-button>
+            </div>
+        </div>
+
+        
+
+    </form>
+    
+
+
+</x-app-layout>
