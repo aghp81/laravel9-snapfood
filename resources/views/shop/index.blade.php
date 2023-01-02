36030ab51ea7f78427a14d70d5513ecd252bccee
abol ghana <aghp81@gmail.com>
2023-01-02 22:43:42 +0330

    دکمه حذف فروشگاه

--- resources/views/shop/index.blade.php
+++ resources/views/shop/index.blade.php
@@ -50,7 +50,17 @@
                             ویرایش
                         </a>
                     </td>
-                    <td> حذف </td>
+                    <td> 
+                        <form action="{{ route('shop.destroy', $shop->id) }}" method="post">
+                            @csrf
+                            @method('DELETE')
+
+                            <button type="submit" class="inline-flex items-center px-4 py-2 bg-red-500 border border-transparent rounded-md font-semibold text-xs text-white uppercase tracking-widest hover:bg-red-700 active:bg-red-900 focus:outline-none focus:border-red-900 focus:ring focus:ring-gray-300 disabled:opacity-25 transition">
+                                حذف
+                            </button>
+
+                        </form>
+                    </td>
                 </tr>
 
             @endforeach
