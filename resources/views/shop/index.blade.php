4545eb58fcce21d835c0fe1186aad05333fccce1
abol ghana <aghp81@gmail.com>
2023-01-02 22:37:09 +0330

    دکمه ویرایش فروشگاه

--- resources/views/shop/index.blade.php
+++ resources/views/shop/index.blade.php
@@ -46,7 +46,7 @@
                     <td> {{ $shop->user->name ?? '-' }} </td>
                     <td> {{ persianDate($shop->created_at) }} </td> <!--  تبدیل تاریخ میلادی به شمسی  -->
                     <td> 
-                        <a href="{{ route ('shop.edit', $shop->id) }}" class="inline-flex items-center px-4 py-2 bg-gray-700 border border-transparent rounded-md font-semibold text-xs text-white uppercase tracking-widest hover:bg-gray-700 active:bg-gray-900 focus:outline-none focus:border-gray-900 focus:ring focus:ring-gray-300 disabled:opacity-25 transition">
+                        <a href="{{ route ('shop.edit', $shop->id) }}" class="inline-flex items-center px-4 py-2 bg-green-500 border border-transparent rounded-md font-semibold text-xs text-white uppercase tracking-widest hover:bg-green-700 active:bg-green-900 focus:outline-none focus:border-green-900 focus:ring focus:ring-gray-300 disabled:opacity-25 transition">
                             ویرایش
                         </a>
                     </td>
