a1e3246ad1f4153897dbe62fc3467e00fdbb25bf
abol ghana <aghp81@gmail.com>
2023-01-06 23:07:37 +0330

    طراحی فرم ایجاد و ویرایش محصول

--- resources/views/product/form.blade.php
+++ resources/views/product/form.blade.php
@@ -6,7 +6,7 @@
     </x-slot>
 
     <!-- اگر شاپ آی دی داشت هدایت شود به آپدیت در غیر این صورت هدایت شود به استور -->
-    <form class="grid grid-cols-3 gap-4" action="{{$product->id ? route('product.update', $product->id) : route('product.store') }}" method="POST">
+    <form action="{{$product->id ? route('product.update', $product->id) : route('product.store') }}" method="POST">
 
         @csrf
         
@@ -16,15 +16,43 @@
             @method('PUT')
         @endif
 
-      
+        <div class="grid grid-cols-12 gap-4">
+
+            <div class="col-span-3">
+                <x-jet-label for="title" value="{{ __('Title Of Product') }}" />
+                <x-jet-input id="title" class="block mt-3 w-full" type="text" name="title" :value="$product->title ?? old('title')" required autofocus />
+            </div>
+
+            <div class="col-span-3">
+                <x-jet-label for="price" value="{{ __('Price Of Product') }}" />
+                <x-jet-input id="price" class="block mt-3 w-full" type="text" name="price" :value="$product->price ?? old('price')" required />
+            </div>
+
+            <div class="col-span-3">
+                <x-jet-label for="discount" value="{{ __('Discount') }}" />
+                <x-jet-input id="discount" class="block mt-3 w-full" type="text" name="discount" :value="$product->discount ?? old('discount')" required />
+            </div>
+
+            <div class="col-span-3">
+                <x-jet-label for="image" value="{{ __('Image') }}" />
+                <input type="file" class="mt-4" name="image" id="image">
+            </div>
+
+            <div class="col-span-12">
+                <x-jet-label for="description" value="{{ __('Description') }}" />
+                <x-jet-input id="description" class="block mt-3 w-full" type="text" name="description" :value="$product->description ?? old('description')" required />
+            </div>
+        
+        </div>
+
         <!-- دکمه ذخیره -->
-        <div  class="col-start-2 col-end-3">
-            <div class="flex justify-center">
+        
+            <div class="flex justify-center mt-4">
                 <x-jet-button>
                     {{ __('Save') }}
                 </x-jet-button>
             </div>
-        </div>
+        
 
         
 
