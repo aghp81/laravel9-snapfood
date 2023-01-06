b54b8bee15611d34ea61a1319a7398d5562f4391
abol ghana <aghp81@gmail.com>
2023-01-06 18:35:24 +0330

    CheckAdmins دسترسی به کاربر ادمین و شاپ

--- app/Http/Middleware/CheckAdmins.php
+++ app/Http/Middleware/CheckAdmins.php
@@ -7,15 +7,17 @@ use Illuminate\Http\Request;
 
 class CheckAdmins
 {
-    /**
-     * Handle an incoming request.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  \Closure(\Illuminate\Http\Request): (\Illuminate\Http\Response|\Illuminate\Http\RedirectResponse)  $next
-     * @return \Illuminate\Http\Response|\Illuminate\Http\RedirectResponse
-     */
+ 
     public function handle(Request $request, Closure $next)
     {
-        return $next($request);
+        $user = auth()->user(); // کاربری که لاگین کرده در حال حاضر رو برمیگردونه.
+        
+        if ( $user && ($user->is('admin') || $user->is('shop') ) ) {
+            
+            return $next($request); // همه چی اوکیه برو مرحله بعد
+        }else{
+            abort(403); // هلپر لاراول برای لغو همه چی
+            // 403 = شما دسترسی این بخش رو ندارید.
+        }
     }
 }
