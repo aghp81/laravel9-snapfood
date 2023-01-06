93e2988b75f32aed6107fefb4e186ca1bdb5f99d
abol ghana <aghp81@gmail.com>
2023-01-06 18:42:07 +0330

    Create - edit - destroy

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -23,8 +23,11 @@ class ProductController extends Controller
  
     public function create()
     {
-        //
+        $product = new Product; // برای اینکه اگر فرم در حالت ایجاد بود همه فیلدها رو نشون بده اگر در حالت ویرایش بود فیلد نام کاربری و ایمیل رو نشون نده.
+        return view('product.form', compact('product')); // برای ایجاد و ویرایش فروشگاه
+        // product در حالت create ای دی ندارد.
     }
+
 
 
     public function store(Request $request)
@@ -34,9 +37,9 @@ class ProductController extends Controller
 
  
    
-    public function edit(product $product)
+    public function edit(Product $product)
     {
-        //
+        return view('product.form', compact('product')); // برای ایجاد و ویرایش فروشگاه
     }
 
 
@@ -48,6 +51,6 @@ class ProductController extends Controller
 
     public function destroy(product $product)
     {
-        //
+        $product->delete();
     }
 }
