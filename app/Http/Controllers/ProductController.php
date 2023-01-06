881155ba5cb1cda4fb2e0df73c14364042b110c2
abol ghana <aghp81@gmail.com>
2023-01-06 18:38:20 +0330

    ساخت index

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -16,7 +16,7 @@ class ProductController extends Controller
  
     public function index()
     {
-        $products = product::all();
+        $products = Product::all();
         return view('product.index', compact('products'));
     }
 
