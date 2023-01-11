c5c9061dcf771e074b8a896e0ce4ffdee9f291e3
abol ghana <aghp81@gmail.com>
2023-01-11 23:42:39 +0330

    محاسبه قیمت محصول پس از تخفیف

--- app/Models/product.php
+++ app/Models/product.php
@@ -9,7 +9,15 @@ class product extends Model
 {
     use HasFactory;
     protected $guarded = ['id']; // فقط id گارد بشه بقیه همه Fillable هستند.
+    protected $appends = ['cost'];// محاسبه قیمت پس از تخفیف
+    
+    // محاسبه قیمت پس از تخفیف
+    public function getCostAttribute()
+    {
+        return $this->price - ( ($this->price * $this->discount) / 100);
+    }
 
+    
     //هر محصول مرتبط به یک فروشگاه است 
     public function shop()
     {
