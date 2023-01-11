d36b67f065c74858a526fcf983820669970fe72f
abol ghana <aghp81@gmail.com>
2023-01-11 23:08:37 +0330

    هر محصول مرتبط به یک فروشگاه است belongsTo

--- app/Models/product.php
+++ app/Models/product.php
@@ -9,4 +9,10 @@ class product extends Model
 {
     use HasFactory;
     protected $guarded = ['id']; // فقط id گارد بشه بقیه همه Fillable هستند.
+
+    //هر محصول مرتبط به یک فروشگاه است 
+    public function shop()
+    {
+        return $this->belongsTo(Shop::class);
+    }
 }
