af7d01e8d6417529f1590e259fc8b39160da9ab2
abol ghana <aghp81@gmail.com>
2023-01-11 23:56:55 +0330

    استفاده از SoftDeletes

--- app/Models/Shop.php
+++ app/Models/Shop.php
@@ -4,10 +4,12 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use illuminate\Database\Eloquent\SoftDeletes;
+
 
 class Shop extends Model
 {
-    use HasFactory;
+    use HasFactory, SoftDeletes;
     protected $guarded = ['id']; // فقط id گارد بشه بقیه همه Fillable هستند.
     
     // استفاده از appends برای نمایش نام و نام خانوادگی در کنار هم
