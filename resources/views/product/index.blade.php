00af3cf1472bf42a092d76f97bf5c371af4cee48
abol ghana <aghp81@gmail.com>
2023-01-13 14:32:23 +0330

    selected مرتب سازی

--- resources/views/product/index.blade.php
+++ resources/views/product/index.blade.php
@@ -54,10 +54,11 @@
         <div class="w-1/4 my-3 px-3">
             <label class="block mb-2" for="">مرتب سازی</label>
             <select class="w-full" name="o" id="">
-                <option value="1">ارزانترین</option>
-                <option value="2">گرانترین</option>
-                <option value="3">جدیدترین</option>
-                <option value="4">قدیمی ترین</option>
+                <option value="">-- انتخاب کنید --</option>
+                <option @if(request('o') == 1) selected    @endif value="1">ارزانترین</option>
+                <option @if(request('o') == 2) selected    @endif value="2">گرانترین</option>
+                <option @if(request('o') == 3) selected    @endif value="3">جدیدترین</option>
+                <option @if(request('o') == 4) selected    @endif value="4">قدیمی ترین</option>
             </select>
         </div>
 
