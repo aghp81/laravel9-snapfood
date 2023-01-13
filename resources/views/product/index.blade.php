a3b0abbc57d238ae07f0ce348630802a988f0ef0
abol ghana <aghp81@gmail.com>
2023-01-13 16:23:03 +0330

    اگر محصول قبلا حذف سافت دلیت شده بود بازیابی شود.

--- resources/views/product/index.blade.php
+++ resources/views/product/index.blade.php
@@ -128,22 +128,40 @@
                             <span class="text-red-500">ندارد</span>    
                         @endif
                     </td>
-                    <td> 
-                        <a href="{{ route ('product.edit', $product->id) }}" class="inline-flex items-center px-4 py-2 bg-green-500 border border-transparent rounded-md font-semibold text-xs text-white uppercase tracking-widest hover:bg-green-700 active:bg-green-900 focus:outline-none focus:border-green-900 focus:ring focus:ring-gray-300 disabled:opacity-25 transition">
-                            ویرایش
-                        </a>
-                    </td>
-                    <td> 
-                        <form action="{{ route('product.destroy', $product->id) }}" method="post">
-                            @csrf
-                            @method('DELETE')
 
-                            <button type="button" class="delete-record inline-flex items-center px-4 py-2 bg-red-500 border border-transparent rounded-md font-semibold text-xs text-white uppercase tracking-widest hover:bg-red-700 active:bg-red-900 focus:outline-none focus:border-red-900 focus:ring focus:ring-gray-300 disabled:opacity-25 transition">
-                                حذف
-                            </button>
+                    <!-- اگر محصول قبلا حذف سافت دلیت شده بود بازیابی شود. -->
+                    @if($product->trashed())
+                        <td colspan="2">
+                            <form action="{{ route('product.destroy', $product->id) }}" method="post">
+                                    @csrf
 
-                        </form>
-                    </td>
+                                    <button type="button" class="delete-record inline-flex items-center px-4 py-2 bg-yellow-500 border border-transparent rounded-md font-semibold text-xs text-white uppercase tracking-widest hover:bg-yellow-700 active:bg-yellow-900 focus:outline-none focus:border-yellow-900 focus:ring focus:ring-gray-300 disabled:opacity-25 transition">
+                                        بازیابی
+                                    </button>
+
+                                </form>
+                        </td>
+
+                        <!-- در غیر این صورت محصول حذف یا ورایش شود. -->
+                    @else
+                        <td> 
+                                <a href="{{ route ('product.edit', $product->id) }}" class="inline-flex items-center px-4 py-2 bg-green-500 border border-transparent rounded-md font-semibold text-xs text-white uppercase tracking-widest hover:bg-green-700 active:bg-green-900 focus:outline-none focus:border-green-900 focus:ring focus:ring-gray-300 disabled:opacity-25 transition">
+                                    ویرایش
+                                </a>
+                            </td>
+                            <td> 
+                                <form action="{{ route('product.destroy', $product->id) }}" method="post">
+                                    @csrf
+                                    @method('DELETE')
+
+                                    <button type="button" class="delete-record inline-flex items-center px-4 py-2 bg-red-500 border border-transparent rounded-md font-semibold text-xs text-white uppercase tracking-widest hover:bg-red-700 active:bg-red-900 focus:outline-none focus:border-red-900 focus:ring focus:ring-gray-300 disabled:opacity-25 transition">
+                                        حذف
+                                    </button>
+
+                                </form>
+                            </td>
+
+                    @endif
                 </tr>
 
             @endforeach
