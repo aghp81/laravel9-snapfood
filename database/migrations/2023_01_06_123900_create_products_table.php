7ab17bae130926a7a992e5eb586b06d9fbb0cca4
abol ghana <aghp81@gmail.com>
2023-01-13 11:00:58 +0330

    برای سافت دیلیت مایگریشن مجزا ایجاد کردیم.

--- database/migrations/2023_01_06_123900_create_products_table.php
+++ database/migrations/2023_01_06_123900_create_products_table.php
@@ -21,7 +21,6 @@ return new class extends Migration
             $table->unsignedSmallInteger('discount')->default(0);
             $table->string('image')->nullable();
             $table->text('description')->nullable();
-            $table->softDeletes();
             $table->timestamps();
         });
     }
