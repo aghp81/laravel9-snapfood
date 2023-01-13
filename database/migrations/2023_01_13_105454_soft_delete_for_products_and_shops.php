cecd6488e476ea95a5594fb0ccf4b42f17d6b28d
abol ghana <aghp81@gmail.com>
2023-01-13 11:02:35 +0330

    همین کار رو برای شاپ هم انجام دادیم.

--- database/migrations/2023_01_13_105454_soft_delete_for_products_and_shops.php
+++ database/migrations/2023_01_13_105454_soft_delete_for_products_and_shops.php
@@ -16,6 +16,10 @@ return new class extends Migration
         Schema::table('products', function (Blueprint $table) {
             $table->softDeletes();
         });
+
+        Schema::table('shops', function (Blueprint $table) {
+            $table->softDeletes();
+        });
     }
 
     /**
@@ -26,6 +30,10 @@ return new class extends Migration
     public function down()
     {
         Schema::table('products', function (Blueprint $table) {
+            $table->dropSoftDeletes();
+        });
+
+        Schema::table('shops', function (Blueprint $table) {
             $table->dropSoftDeletes();
         });
     }
