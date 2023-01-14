be76bc3beaeb3645c0b5da7d8704483fa636c4cc
abol ghana <aghp81@gmail.com>
2023-01-14 13:47:54 +0330

    checkPolicy

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -120,7 +120,7 @@ class ProductController extends Controller
    
     public function edit(Product $product)
     {
-        checkProduct($product); // هر فروشنده فقط دسترسی ویرایش محصول به خود را دارد.
+        checkPolicy('product', $product); // هر فروشنده فقط دسترسی ویرایش محصول به خود را دارد.
         $shops = Shop::all(); // نمایش همه شاپ ها در پنل ادمین برای افزودن محصول
         return view('product.form', compact('product', 'shops')); // برای ایجاد و ویرایش فروشگاه
     }
@@ -128,7 +128,7 @@ class ProductController extends Controller
 
     public function update(Request $request, product $product)
     {
-        checkProduct($product); // هر فروشنده فقط دسترسی ویرایش محصول به خود را دارد.
+        checkPolicy('', $product); // هر فروشنده فقط دسترسی ویرایش محصول به خود را دارد.
         $data = $request->validate($this->validationRules); // ولدیشین ها
         if (isset($data['image']) && $data['image']) { // اگر دیتای ایمیج داشتیم
             $data['image'] = upload($data['image']); // آپلود تصویر ا استفاده از تابع هلپر
@@ -150,7 +150,7 @@ class ProductController extends Controller
     // بازیابی محصول سافت دیلیت شده
     public function restore($id)
     {
-        checkProduct($product); // هر فروشنده فقط دسترسی ری استور محصول به خود را دارد.
+        checkPolicy('', $product); // هر فروشنده فقط دسترسی ری استور محصول به خود را دارد.
         $product = Product::withTrashed()->where('id', $id)->firstOrFail();
         $product->restore();
         return redirect()->route('product.index')->withMessage( __('SUCCESS') );
@@ -159,7 +159,7 @@ class ProductController extends Controller
 
     public function destroy(product $product)
     {
-        checkProduct($product); // هر فروشنده فقط دسترسی حذف محصول به خود را دارد.
+        checkPolicy('', $product); // هر فروشنده فقط دسترسی حذف محصول به خود را دارد.
         $product->delete();
         return redirect()->route('product.index')->withMessage( __('DELETED') ); // DELETED in fa.json
     }
