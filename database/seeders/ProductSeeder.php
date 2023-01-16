bf8ebb4e3ad80bea9584edf70e2f8a60f6626712
abol ghana <aghp81@gmail.com>
2023-01-17 00:55:40 +0330

    ایجاد سیدر برای محصولات

--- database/seeders/ProductSeeder.php
+++ database/seeders/ProductSeeder.php
@@ -4,6 +4,10 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Str;
+
+
 
 class ProductSeeder extends Seeder
 {
@@ -14,6 +18,15 @@ class ProductSeeder extends Seeder
      */
     public function run()
     {
-        //
+        $date = now()->add( -rand(1, 30), 'day'); // ایجاد تاریخ رندم - از یک تا سی روز از تاریخ امروز کم می کند.
+        DB::table('products')->insert([
+            'shop_id' => rand(9, 11),
+            'title' => Str::random(30),
+            'price' => rand(10, 100) * 1000,
+            'discount' => rand(0, 5) * 5,
+            'description' => Str::random(rand(1, 4) * 50),
+            'created_at' => $date,
+            'updated_at' => $date,
+        ]);
     }
 }
