3f145a5da8d73f542895d876409d5745e6e45831
abol ghana <aghp81@gmail.com>
2023-01-18 13:17:15 +0330

    جستجوی محصولات توسط کاربر

--- app/Http/Controllers/LandingController.php
+++ app/Http/Controllers/LandingController.php
@@ -21,8 +21,30 @@ class LandingController extends Controller
 
     public function products()
     {
+        // جستجوی محصولات
+        $products = Product::query();
+
+        // جستجو بر اساس نام محصول
+        if ($p = request('p')) {
+            $products = $products->where('title', 'like', "%$p%");
+        }
+
+        // جستجو بر اساس مرتب سازی
+        if ($o = request('o')) {
+            if ($o == 1) {
+                $products = $products->latest(); 
+            }
+            if ($o == 2) {
+                $products = $products->orderBy('price', 'ASC'); 
+            }
+            if ($o == 3) {
+                $products = $products->orderBy('price', 'DESC'); 
+            }
+        }
+
+
         // نمایش محصولات در صفحه landing
-        $products = Product::paginate(9);
+        $products = $products->paginate(9);
         return view('landing.products', compact('products'));
     }
 
