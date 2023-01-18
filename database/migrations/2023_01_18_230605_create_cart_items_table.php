7d2a642c6906b0b13b4bcc19699513ec3150b58d
abol ghana <aghp81@gmail.com>
2023-01-18 23:15:56 +0330

    جدول CartItem

--- database/migrations/2023_01_18_230605_create_cart_items_table.php
+++ database/migrations/2023_01_18_230605_create_cart_items_table.php
@@ -15,6 +15,10 @@ return new class extends Migration
     {
         Schema::create('cart_items', function (Blueprint $table) {
             $table->id();
+            $table->unsignedInteger('cart_id'); // محصولات سبد خرید متعلق به چه سبد خریدی است؟
+            $table->unsignedInteger('product_id'); 
+            $table->unsignedSmallInteger('count'); // تعداد هر محصول در سبد خرید
+            $table->unsignedBigInteger('payable'); // مشخص کردن قیمت نهایی محصول. ممکن است محصول مدتی در سبد خرید کاربر بماند و قیمتش تغییر کند.
             $table->timestamps();
         });
     }
