f564c2afa3380af5061533e9f3d1ffa376438d7d
abol ghana <aghp81@gmail.com>
2023-01-19 21:49:32 +0330

    اگر کاربر لاگین بود و در سبد خرید محصولی داشت

--- app/Models/product.php
+++ app/Models/product.php
@@ -26,4 +26,18 @@ class product extends Model
     {
         return $this->belongsTo(Shop::class);
     }
+
+    // آیا یک محصول از قبل در کارت خرید وجود دارد؟
+    public function isInCart()
+    {
+        $currentLogedInUser = auth()->user(); // باید ایتدا ببینیم کاربر لاگین کرده که بتوان سبد خرید او را بررسی کرد.
+        // اگر کاربر لاگین کرده بود
+        if ($currentLogedInUser) {
+            $cart = Cart::where('user_id', $currentLogedInUser->id)->first(); // اگر کاربری داشتیم کارت رو پیدا کنه.
+            // اگر کارتی پیدا کرد
+            if ($cart) {
+               return CartItem::where('cart_id', $cart->id)->where('product_id', $this->id)->first(); // $this->id == $product->id چون در مدل پروداکت هستیم. first(); == اگر پیدا کرد کارت ایتم رو برگردونه
+            }
+        }
+    }
 }
