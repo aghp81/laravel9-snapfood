388fd5993a05abafa5bccce0f8e1cf43098052e0
abol ghana <aghp81@gmail.com>
2023-01-20 10:16:09 +0330

    نمایش نام محصول در سبد خرید

--- resources/views/landing/cart.blade.php
+++ resources/views/landing/cart.blade.php
@@ -7,8 +7,26 @@
     <hr class="mt-3">
 
     <!-- جدول نمایش محتویات سبد خرید -->
-    <table class="table">
+    <table class="table table-bordered">
+        <thead>
+            <tr>
+                <th> ردیف </th>
+                <th> محصول </th>
+                <th> تعداد </th>
+                <th> قابل پرداخت </th>
+            </tr>
+        </thead>
 
+        <tbody>
+            <!-- LandingController cart method -->
+            <!-- each cart has many cart items -->
+            @foreach ($cart->items as $key => $item)
+            <tr>
+                <th> {{ $key+1 }} </th>
+                <td> {{ $item->product->title ?? '--' }} </td> <!-- CartItem.php - public function product() --> 
+            </tr>
+            @endforeach
+        </tbody>
     </table>
 
 @endsection
