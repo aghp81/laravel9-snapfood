9b5befe2d072e6ee6784c6e72ba213dfb2942e48
abol ghana <aghp81@gmail.com>
2023-01-20 09:56:45 +0330

    ایجاد رابطه has many بین کارت و کارت آیتم one to many

--- app/Models/Cart.php
+++ app/Models/Cart.php
@@ -9,4 +9,10 @@ class Cart extends Model
 {
     use HasFactory;
     protected $guarded = ['id'];
+
+    // ارتباط بین کارت و کارت آیتم. هر کارت تعداد زیادی کارت آیتم دارد.
+    public function items()
+    {
+        return $this->hasMany(CartItem::class);
+    }
 }
