f6c1a27f57d0d2dc61fbffd6880d11a981bb371d
abol ghana <aghp81@gmail.com>
2023-02-18 10:04:32 +0330

    ایجاد کنترلر سفارشات

---
+++ app/Http/Controllers/OrderController.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class OrderController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    
+
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
+    }
+}
