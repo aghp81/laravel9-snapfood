c0921f101a92ce439706f81a172a5a89aa218807
abol ghana <aghp81@gmail.com>
2023-02-18 19:38:13 +0330

    دکمه بازگشت به صفحه سفارشات

--- resources/views/order/show.blade.php
+++ resources/views/order/show.blade.php
@@ -5,6 +5,15 @@
         </h2>
     </x-slot>
 
+    <!-- افزودن محصولات دکمه -->
+    <div class="flex justify-end mb-5">
+
+        <a href="{{ route ('order.index') }}" class="inline-flex items-center px-4 py-2 bg-gray-700 border border-transparent rounded-md font-semibold text-xs text-white uppercase tracking-widest hover:bg-gray-700 active:bg-gray-900 focus:outline-none focus:border-gray-900 focus:ring focus:ring-gray-300 disabled:opacity-25 transition">
+            {{ __('بازگشت به لیست سفارشات  ') }}
+        </a>
+
+    </div>
+
     @include('landing.fragments.cart_table', ['cart' => $order, 'operations' => false])
 
     
