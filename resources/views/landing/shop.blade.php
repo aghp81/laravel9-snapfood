2214603bac7b1e41e4022ba28bc6ae425e55e0e1
abol ghana <aghp81@gmail.com>
2023-02-28 16:06:20 +0330

    فرم کامنت گذاری

--- resources/views/landing/shop.blade.php
+++ resources/views/landing/shop.blade.php
@@ -18,4 +18,17 @@
     
     <hr>
     {{ $products->links() }}
+
+    <!-- کامنت گذاری -->
+    <div>
+        <h2 class="text-center mt-3"> لیست کامنت ها </h2>
+        <hr>
+        <h4 class="my-3">در صورت تمایل می توانید کامنت بگذارید</h4>
+        <form action="" method="post">
+            <textarea name="text" class="form-control my-2" id="" rows="4" placeholder="متن کامنت"></textarea>
+            <div class="text-center">
+                <button type="submit" class="btn btn-primary text-primary mt-3">تایید</button>
+            </div>
+        </form>
+    </div>
 @endsection
