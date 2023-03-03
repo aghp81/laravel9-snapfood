f2117a4adc55b51cb60e4c8b814a62425f0de916
abol ghana <aghp81@gmail.com>
2023-03-03 09:06:45 +0330

    ایجاد مایگریشن و کنترلر برای کامنت ها

--- app/Http/Controllers/CommentController.php
+++ app/Http/Controllers/CommentController.php
@@ -3,8 +3,18 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Comment;
 
 class CommentController extends Controller
 {
-    //
+    public function store(Request $request)
+    {
+        // dd($request->all());
+        Comment::create([
+            'shop_id' => $request->shop_id,
+            'text' => $request->text
+        ]);
+
+        return back()->withMessage('کامنت شما با موفقیت ارسال شد.');
+    }
 }
