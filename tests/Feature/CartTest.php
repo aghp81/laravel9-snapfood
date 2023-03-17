0cda34b45a310e2f33a8066e2b231f2b79689750
aghp81 <aghp81@gmail.com>
2023-03-17 10:04:53 -0700

    تست لود شدن سبد خرید

--- tests/Feature/CartTest.php
+++ tests/Feature/CartTest.php
@@ -9,14 +9,14 @@ use Tests\TestCase;
 class CartTest extends TestCase
 {
     /**
-     * A basic feature test example.
+     * Test if user can see cart page.
      *
      * @return void
      */
-    public function test_example()
+    public function test_if_cart_pages_loads()
     {
-        $response = $this->get('/');
+        $response = $this->get('landing/cart');
 
-        $response->assertStatus(200);
+        $response->assertStatus(200); // اگر استاتوس کد 200 بود یعنی صفحه با موفقیت لود شده.
     }
 }
