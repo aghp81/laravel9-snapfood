0993a2e2a54216de76ce76bed7b274de4f7006b6
aghp81 <aghp81@gmail.com>
2023-03-17 17:11:33 -0700

    تست: آیا کاربر می تواند در کارت ادد کند؟

--- tests/Feature/CartTest.php
+++ tests/Feature/CartTest.php
@@ -5,6 +5,7 @@ namespace Tests\Feature;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
+use App\Models\User;
 
 class CartTest extends TestCase
 {
@@ -16,6 +17,21 @@ class CartTest extends TestCase
     public function test_if_cart_pages_loads()
     {
         $response = $this->get('landing/cart');
+
+        $response->assertStatus(200); // اگر استاتوس کد 200 بود یعنی صفحه با موفقیت لود شده.
+    }
+
+    /**
+     * Test if user can add to cart.
+     *
+     * @return void
+     */
+    public function test_if_users_can_add_to_cart()
+    {
+        $user = User::factory()->create(); // ایجاد یک یوزر برای تست 
+        $response = $this->actingAs($user)->post('cart/manage/1',[
+            'type' => 'add'
+        ]);
 
         $response->assertStatus(200); // اگر استاتوس کد 200 بود یعنی صفحه با موفقیت لود شده.
     }
